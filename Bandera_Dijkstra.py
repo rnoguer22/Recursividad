68a4f3b6819b97640f3837ba0b7b90e5f1e9b593
rnoguer22 <rnogugon@myuax.com>
2022-02-27 22:43:23 +0100

    Definimos la funcion "ordenar_"

--- Bandera_Dijkstra.py
+++ Bandera_Dijkstra.py
@@ -2,3 +2,15 @@ class Bandera:
     #Definimos el constructor
     def __init__(self, bandera):
         self.bandera = bandera
+    
+    def ordenar_(self):
+        #Definimos las listas donde se almacenaran los colores de cada tipo
+        r = []
+        g = []
+        b = []
+        if self.bandera == "R":
+            r.append(self.bandera)
+        elif self.bandera == "G":
+            g.append(self.bandera)
+        elif self.bandera == "B":
+            b.append(self.bandera)
