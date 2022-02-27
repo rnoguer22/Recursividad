0fdac2478e68071e2580eeaf88d5bc7683743735
rnoguer22 <rnogugon@myuax.com>
2022-02-27 01:05:17 +0100

    Funcion es_albatetico Esta funcion nos devuelve True si el caracter es del alfabeto. False en caso contario

--- Palindromos.py
+++ Palindromos.py
@@ -3,4 +3,10 @@ class Palindromo:
     def __init__(self, frase):
         self.frase = frase
     
-    
+    #Funcion para saber si un caracter es del alfabeto
+    def es_alfabetico(self, car):
+        return car.isupper() or car.islower()
+
+    def alfanumerico(self):
+        if self.frase == False:
+            return self.frase
