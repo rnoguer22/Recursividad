c2636431831ddc964d0dfbeea20965ab4c6d25b0
rnoguer22 <rnogugon@myuax.com>
2022-02-27 22:53:45 +0100

    Completamos la funcion "ordenar_"

--- Bandera_Dijkstra.py
+++ Bandera_Dijkstra.py
@@ -3,14 +3,24 @@ class Bandera:
     def __init__(self, bandera):
         self.bandera = bandera
     
-    def ordenar_(self):
+    def ordenar_(self, n):
         #Definimos las listas donde se almacenaran los colores de cada tipo
         r = []
         g = []
         b = []
-        if self.bandera == "R":
-            r.append(self.bandera)
-        elif self.bandera == "G":
-            g.append(self.bandera)
-        elif self.bandera == "B":
-            b.append(self.bandera)
+        #Añadimos un contador que nos ayudara a recorrer "bandera"
+        n = 0
+        if self.bandera[n] == "R":
+            r.append(self.bandera[n])
+            n += 1
+            self.ordenar_(self, n)
+        elif self.bandera[n] == "G":
+            g.append(self.bandera[n])
+            n += 1
+            self.ordenar_(self, n)
+        elif self.bandera[n] == "B":
+            b.append(self.bandera[n])
+            n += 1
+            self.ordenar_(self, n)
+        else:
+            return [r, g, b]
